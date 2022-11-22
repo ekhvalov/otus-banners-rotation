0e6c5c2043c74739aa52672c4ab227017a99781b
user <ekhvalov@gmail.com>
2022-11-22 10:47:49 +0900

    Storage interface defined.

---
+++ internal/app/interfaces.go
@@ -0,0 +1,16 @@
+package app
+
+import "context"
+
+type Storage interface {
+	CreateBanner(ctx context.Context, description string) (id string, err error)
+	DeleteBanner(ctx context.Context, id string) error
+	CreateSlot(ctx context.Context, description string) (id string, err error)
+	DeleteSlot(ctx context.Context, id string) error
+	CreateSocialGroup(ctx context.Context, description string) (id string, err error)
+	DeleteSocialGroup(ctx context.Context, id string) error
+	AttachBanner(ctx context.Context, slotID, bannerID string) error
+	DetachBanner(ctx context.Context, slotID, bannerID string) error
+	SelectBanner(ctx context.Context, slotID, socialGroupID string) (bannerID string, err error)
+	ClickBanner(ctx context.Context, slotID, bannerID, socialGroupID string) error
+}
